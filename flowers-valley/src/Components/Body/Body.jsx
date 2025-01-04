00a6efa41ece33a241c71c50ce8fa11d67218a95
Felipe Cassiano <felipe3.cassiano@gmail.com>
2025-01-04 12:47:19 -0300

    Adicionado comentários

--- flowers-valley/src/Components/Body/Body.jsx
+++ flowers-valley/src/Components/Body/Body.jsx
@@ -9,10 +9,14 @@ function Body() {
     return(
         <div className="Body-container">
 
-        <img className="Body-img" src="https://placehold.co/100" alt="" />
+        {/* Foto de Perfil */}
+        <img className="Body-img" src="https://placehold.co/100" alt="" /> 
+        {/* Título */}
         <div className="Body-text">Flower's Valley</div>
 
+        {/* Lista de redes sociais */}
         <ul className="Body-socials">
+            {/* Links de cada rede social, com seu ícone */}
             <li>
                 <a className="Body-links" href="https://www.instagram.com/flowersvalleyatelie/" target="_blank">
                     <FontAwesomeIcon icon={faInstagram} size="2x"/>
@@ -20,6 +24,13 @@ function Body() {
             </li>
         </ul>
 
+        {
+        //? Componentes do Link que incluem:
+        
+        //*Link que redireciona para o site específico
+        //*Imagem de cada link, dando um preview do que se trata
+        //*Texto, indicando qual link está direcionando
+        }
         <Links 
         href="https://legacy.reactjs.org/docs/components-and-props.html"
         img="/cloud.png"
